4cd9ec43a0a09e218a2188f5b93fb721bb0e085c
bds18b <bds18b@my.fsu.edu>
2023-10-26 18:25:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const firstpageanimation = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const slidefromleftanimation = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide-from-left-show");
+    } else {
+      entry.target.classList.remove("slide-from-left-show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => firstpageanimation.observe(el));
+
+const hiddenElements1 = document.querySelectorAll(".slide-from-left-hidden");
+hiddenElements1.forEach((el) => slidefromleftanimation.observe(el));
